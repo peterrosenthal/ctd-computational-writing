6cfa96e1005b7f927ee8267cec3ba4778ba481b4
Peter Rosenthal <peter@rosenth.al>
2021-02-21 18:25:31 -0700

    create simple speed setting for reading machine
    all of the previous speed settings combined into one, but still available under "advanced settings"

--- reading-machine/reading-machine.js
+++ reading-machine/reading-machine.js
@@ -4,11 +4,11 @@ import Settings from "./settings.js";
 
 const textForTesting = ["this", "is", "some", "test", "text", " ", " ", " "];
 
-const sketch = new Sketch(document.getElementById("sketch"), window.innerWidth, window.innerHeight);
+const sketch = new Sketch(document.getElementById("sketch"));
 const settings = new Settings(sketch);
 
 const addText = (index) => {
-    const newText = new FlyingText(textForTesting[index], settings.textSpeed, settings.textAcceleration, window.innerWidth, window.innerHeight);
+    const newText = new FlyingText(textForTesting[index], settings.textSpeed, settings.textAcceleration, sketch.width, sketch.height);
     const timeout = 1000 / settings.textFrequency;
     index = (index + 1) % textForTesting.length;
     
--- reading-machine/settings.js
+++ reading-machine/settings.js
@@ -1,22 +1,52 @@
 export default class Settings {
     constructor(sketch) {
+        const showSettingsButton = document.getElementById("showSettings");
+        const settingsTypeCheckbox = document.getElementById("settingsType");
+        const simpleSettingsSection = document.getElementById("simpleSettings");
+        const advancedSettingsSection = document.getElementById("advancedSettings");
+
+        showSettingsButton.addEventListener("click", () => {
+            formObject.style.display = "block";
+        });
+
+        settingsTypeCheckbox.addEventListener("click", () => {
+            if (settingsTypeCheckbox.checked === true) {
+                simpleSettingsSection.style.display = "none";
+                advancedSettingsSection.style.display = "block";
+            } else {
+                simpleSettingsSection.style.display = "block";
+                advancedSettingsSection.style.display = "none";
+            }
+        })
+
         const formObject = document.getElementById("settings");
         const textSpeedObject = document.getElementById("textSpeed");
         const textAccelerationObject = document.getElementById("textAcceleration");
         const textFrequencyObject = document.getElementById("textFrequency");
+        const simpleSpeedObject = document.getElementById("simpleSpeed");
         const fontSizeObject = document.getElementById("fontSize");
         const bgColorObject = document.getElementById("backgroundColor");
         const fgColorObject = document.getElementById("foregroundColor");
 
         formObject.addEventListener("submit", (e) => {
             e.preventDefault();
-            this.textSpeed = textSpeedObject.value;
-            this.textAcceleration = textAccelerationObject.value;
-            this.textFrequency = textFrequencyObject.value;
+            if (settingsTypeCheckbox.checked === true) {
+                this.textSpeed = textSpeedObject.value;
+                this.textAcceleration = textAccelerationObject.value;
+                this.textFrequency = textFrequencyObject.value;
+            } else {
+                this.textSpeed = 5 + simpleSpeedObject.value / 6;
+                this.textAcceleration = 3 + simpleSpeedObject.value / 20;
+                this.textFrequency = 1 + simpleSpeedObject.value / 10;
+                textSpeedObject.value = this.textSpeed;
+                textAccelerationObject.value = this.textAcceleration;
+                textFrequencyObject.value = this.textFrequency;
+            }
             sketch.fontSize = fontSizeObject.value;
             sketch.backgroundColor = bgColorObject.value;
             sketch.foregroundColor = fgColorObject.value;
         });
+
         formObject.addEventListener("reset", (e) => {
             e.preventDefault();
             textSpeedObject.value = this.textSpeed;
@@ -25,11 +55,26 @@ export default class Settings {
             fontSizeObject.value = sketch.fontSize;
             bgColorObject.value = sketch.backgroundColor;
             fgColorObject.value = sketch.foregroundColor;
+
+            formObject.style.display = "none";
         });
 
-        this.textSpeed = textSpeedObject.value;
-        this.textAcceleration = textAccelerationObject.value;
-        this.textFrequency = textFrequencyObject.value;
+        if (settingsTypeCheckbox.checked === true) {
+            simpleSettingsSection.style.display = "none";
+            advancedSettingsSection.style.display = "block";
+            this.textSpeed = textSpeedObject.value;
+            this.textAcceleration = textAccelerationObject.value;
+            this.textFrequency = textFrequencyObject.value;
+        } else {
+            simpleSettingsSection.style.display = "block";
+            advancedSettingsSection.style.display = "none";
+            this.textSpeed = 5 + simpleSpeedObject.value / 6;
+            this.textAcceleration = 3 + simpleSpeedObject.value / 20;
+            this.textFrequency = 1 + simpleSpeedObject.value / 10;
+            textSpeedObject.value = this.textSpeed;
+            textAccelerationObject.value = this.textAcceleration;
+            textFrequencyObject.value = this.textFrequency;
+        }
         sketch.fontSize = fontSizeObject.value;
         sketch.backgroundColor = bgColorObject.value;
         sketch.foregroundColor = fgColorObject.value;
