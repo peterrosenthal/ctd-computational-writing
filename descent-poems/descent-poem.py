4a256fe06ad1dccddd34f5df1f76bc2896a6893b
Peter Rosenthal <peter@rosenth.al>
2021-03-28 16:16:06 -0600

    Small improvements to the descent poems

--- descent-poems/descent-poem.py
+++ descent-poems/descent-poem.py
@@ -1,6 +1,7 @@
 import gensim
 from gensim.models import Word2Vec
 import nltk
+import numpy as np
 import math
 import multiprocessing
 import random
@@ -33,9 +34,9 @@ def make_training_data(filename, use_stop_words):
             out.write(preprocess_text(sentence.lower()) + '\n')
     return out_name
 
-def train_word2vec(filename):
+def train_word2vec(filename, vector_length):
     data = gensim.models.word2vec.LineSentence(filename)
-    return Word2Vec(data, size=10, window=5, min_count=1, workers=multiprocessing.cpu_count())
+    return Word2Vec(data, size=vector_length, window=5, min_count=1, workers=multiprocessing.cpu_count())
 
 def get_top_n_neighbors(model, word, n):
     words = []
@@ -57,42 +58,43 @@ def get_vector_magnitude(vector):
 
 def get_descent_neighbor(model, word, target, n):
     words = get_top_n_neighbors(model, word, n)
-    lowest_word = ''
-    lowest_word_mag = float('inf')
-    for similar_word in words:
-        magnitude = get_vector_magnitude(model.wv[similar_word] - target)
-        if magnitude < lowest_word_mag:
-            lowest_word_mag = magnitude
-            lowest_word = similar_word
-    return lowest_word
+    magnitudes = np.zeros(len(words))
+    for word_index, similar_word in enumerate(words):
+        magnitudes[word_index] = get_vector_magnitude(model.wv[similar_word] - target)
+    # print_top_k_words_and_magnitudes(10, words, magnitudes)
+    return words[np.argmin(magnitudes)]
 
-def get_average_word(model):
+def get_average_word(model, vector_length):
     avg_word = 0
     for word_vector in model.wv.vectors:
         avg_word += word_vector
-    avg_word /= len(model.wv.vectors)
+    avg_word /= (len(model.wv.vectors) / vector_length)
     return avg_word
-        
+
+def print_top_k_words_and_magnitudes(k, words, magnitudes):
+    k = min(k, len(words) - 1)
+    for index in np.argpartition(magnitudes, k)[:k]:
+        print(words[index] + ': ' + str(magnitudes[index]))
+    print(words[np.argmin(magnitudes)])
 
 def main():
+    vector_length = 75
     filename = make_training_data('corpus.txt', True)
-    model = train_word2vec(filename)
-    target = get_average_word(model)
-    word = word_from_vec(model, model.trainables.seeded_vector(random.random() * 10000, vector_size=10))
+    model = train_word2vec(filename, vector_length)
+    target = get_average_word(model, vector_length)
+    word = word_from_vec(model, model.trainables.seeded_vector(random.random() * 10000, vector_size=vector_length))
     if len(sys.argv) > 1:
         word = sys.argv[1]
     poem = word
     num_words = random.randrange(1, 25)
-    neighbors = random.randrange(5, 25)
+    neighbors = random.randrange(50, 500)
     while num_words > 0:
         word = get_descent_neighbor(model, word, target, neighbors)
         if word in poem:
             neighbors *= 2
-            print(neighbors)
         poem += ' ' + word
         num_words -= 1
     print(poem)
-    print(word_from_vec(model, target))
 
 if __name__ == '__main__':
     main()
