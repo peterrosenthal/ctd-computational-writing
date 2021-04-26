80c93c2c5d130836c4532839c1b0b3afda94564c
Peter Rosenthal <peter@rosenth.al>
2021-04-26 14:17:10 -0600

    Add first version of final proj parse_tribunal.py

---
+++ tribunal/README.md
@@ -0,0 +1,7 @@
+# GPT-2 Plays League of Legends
+
+The goal of this project is to recreate a chat log of a League of Legends game using the GPT-2 neural network.
+
+## Data
+
+The model was trained on a set of in-game chat logs, taken from the Tribunal, a no longer operational attempt at a crowd sourced player behavior management system. I downloaded the data from [Sim Sheng Xue on Kaggle](https://www.kaggle.com/simshengxue/league-of-legends-tribunal-chatlogs) who did not specify how they managed to get their hands on the data in the first place.
---
+++ tribunal/parse_tribunal.py
@@ -0,0 +1,75 @@
+import json
+import random
+
+class SummonerNameGenerator():
+  prefixes = [
+    '', '', '', '', '', '', '', '', '',
+    'xX', 'Xx', 'xx',
+    'C9 ', 'TSM ', 'SKT '
+  ]
+  name_part1s = [
+    'Tryhard', 'Cursed', 'Sad', 'E', 'ez',
+    'Slayer', 'Star', 'Tiny', 'Sad',
+  ]
+  name_part2s = [
+    '', '', '', '',
+    'Egg', 'World', 'Cat', 'Boy', 'Girl',
+    'pz', 'Star', 'Lord', 'Sloth'
+  ]
+  suffixes = [
+    '', '', '', '',
+    '', '', '', '',
+    '', '', '', '',
+    'Xx', 'xX', 'xx'
+  ]
+
+  def new_name(self):
+    prefix = self.prefixes[random.randrange(0, len(self.prefixes))]
+    name_part1 = self.name_part1s[random.randrange(0, len(self.name_part1s))]
+    name_part2 = self.name_part2s[random.randrange(0, len(self.name_part2s))]
+    suffix = self.suffixes[random.randrange(0, len(self.suffixes))]
+    return prefix + name_part1 + name_part2 + suffix
+
+if __name__ == '__main__':
+  with open('tribunal.txt', 'w') as out_file:
+    for i in range(1, 10059):
+      try:
+        with open('cases/' + str(i) + '.txt', 'r') as in_file:
+          data = in_file.read()
+      except FileNotFoundError:
+        pass
+
+      json_obj = json.loads(data)
+
+      summoner_name_generator = SummonerNameGenerator()
+      summoner_names = {}
+
+      out_file.write('<|startoftext|>\n')
+
+      for chat_obj in json_obj['chat_log']:
+        timestamp = chat_obj['time']
+        all_chat = ''
+        if chat_obj['sent_to'] == 'All':
+          all_chat  = '[All] '
+          include = True
+        elif chat_obj['association_to_offender'] != 'enemy':
+          include = True
+        else:
+          include = False
+        champion_name = chat_obj['champion_name']
+        try:
+          summoner_name = summoner_names[champion_name]
+        except KeyError:
+          summoner_name = summoner_name_generator.new_name()
+          summoner_names[champion_name] = summoner_name
+        message = chat_obj['message']
+
+        if include:
+          out_file.write('[' + timestamp + '] ')
+          out_file.write(all_chat)
+          out_file.write(summoner_name)
+          out_file.write(' (' + champion_name + ')')
+          out_file.write(': ' + message)
+          out_file.write('\n')
+
+      out_file.write('<|endoftext|>\n')
